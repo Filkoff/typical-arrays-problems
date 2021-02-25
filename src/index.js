724e9ad873b9a724ea7e5beefa7c725a12b67f9c
github-usernameFilkoff <filkovalevich@gmail.com>
2021-02-25 23:00:11 +0200

    init: start array-problem task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+   if (!array || array.length===0) {
+        return 0
+    } else {
+    let m=array[0]
+    for (let item of array) {
+        if (m > item) {
+            m = item
+        }
+    }
+        return m
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length===0) {
+        return 0
+    } else {
+        let m=array[0]
+        for (let item of array) {
+            if (m < item) {
+                m = item
+            }
+        }
+        return m}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length===0) {
+        return 0
+  } else {
+    let sum = 0 
+        for (let item of array) {
+            sum+=item
+        }
+    return sum / array.length
+  }
 }
+
+
